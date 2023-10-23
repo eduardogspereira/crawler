6c10780f3e7aedaea29f480630bb249f4ff1ec00
majortom <Eduardo G. S. Pereira>
2023-10-23 21:21:36 +0100

    chore: integrate crawler code with worker

--- crawler.go
+++ crawler.go
@@ -2,50 +2,100 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 type Crawler struct {
 	httpClient *http.Client
 
-	pageVisited map[string]bool
+	linksForTargetURLResult []*LinksForTargetURL
+	errorForTargetURLResult []*ErrorForTargetURL
+	pageVisited             map[string]bool
+	workerPool              *WorkerPool
+	m                       sync.Mutex
 }
 
 func NewCrawler(httpClient *http.Client) *Crawler {
 	return &Crawler{
 		httpClient:  httpClient,
 		pageVisited: make(map[string]bool),
+		workerPool:  NewWorkerPool(100), //!TODO: set number of workers as a setting
 	}
 }
 
-func (c *Crawler) GetAllLinksFor(ctx context.Context, targetURL *url.URL) {
-	workerPool := NewWorkerPool(100) //!TODO: set number of workers as a setting
-	workerPool.AddTask(targetURL)
+func (c *Crawler) GetAllLinksFor(ctx context.Context, targetURL *url.URL) ([]*LinksForTargetURL, []*ErrorForTargetURL) {
+	c.MarkPageAsVisited(targetURL)
+	c.workerPool.AddTask(targetURL)
+
+	c.workerPool.ProcessTasks(func(nextTask interface{}) {
+		nextTargetURL := nextTask.(*url.URL)
+		linksForTargetURL, errorForTargetURL := c.TaskGetLinksForURL(ctx, nextTargetURL)
+		c.m.Lock()
+		defer c.m.Unlock()
+		if errorForTargetURL != nil {
+			c.errorForTargetURLResult = append(c.errorForTargetURLResult, errorForTargetURL)
+			return
+		}
+		c.linksForTargetURLResult = append(c.linksForTargetURLResult, linksForTargetURL)
+	})
+
+	return c.linksForTargetURLResult, c.errorForTargetURLResult
 }
 
-type LinksForURLResult struct {
+type LinksForTargetURL struct {
 	links     []*url.URL
 	targetURL *url.URL
 }
 
-func (c *Crawler) TaskGetLinksForURL(ctx context.Context, targetURL *url.URL, linksForURLResults chan<- *LinksForURLResult, failedURLs chan<- *url.URL) {
+// !TODO: verify if it makes more sense to build a struct error
+type ErrorForTargetURL struct {
+	err       error
+	targetURL *url.URL
+}
+
+func (c *Crawler) TaskGetLinksForURL(ctx context.Context, targetURL *url.URL) (*LinksForTargetURL, *ErrorForTargetURL) {
 	request, err := http.NewRequestWithContext(ctx, http.MethodGet, targetURL.String(), nil)
 	if err != nil {
-		failedURLs <- targetURL
-		return
-		//return nil, fmt.Errorf("failed to build request: %w", err) //!TODO: ADD DEBUG LOG
+		return nil, &ErrorForTargetURL{
+			err:       fmt.Errorf("failed to build request: %w", err),
+			targetURL: targetURL,
+		}
 	}
 
 	response, err := c.httpClient.Do(request)
 	if err != nil {
-		failedURLs <- targetURL
-		return
-		//return nil, fmt.Errorf("failed to do request: %w", err) //!TODO: ADD DEBUG LOG
+		return nil, &ErrorForTargetURL{
+			err:       fmt.Errorf("failed to build request: %w", err),
+			targetURL: targetURL,
+		}
 	}
 
-	linksForURLResults <- &LinksForURLResult{
-		targetURL: targetURL,
-		links:     FilterURLsBySubdomain(targetURL, ExtractLinksFrom(response.Body)),
+	linksForTargetURL := FilterURLsBySubdomain(targetURL, ExtractLinksFrom(response.Body))
+	for _, l := range linksForTargetURL {
+		if !c.PageAlreadyVisited(l) {
+			c.MarkPageAsVisited(l)
+			c.workerPool.AddTask(l)
+		}
 	}
+
+	return &LinksForTargetURL{
+		targetURL: targetURL,
+		links:     linksForTargetURL,
+	}, nil
+}
+
+func (c *Crawler) MarkPageAsVisited(targetURL *url.URL) {
+	c.m.Lock()
+	defer c.m.Unlock()
+	c.pageVisited[targetURL.Host+targetURL.Path] = true
+}
+
+func (c *Crawler) PageAlreadyVisited(targetURL *url.URL) bool {
+	c.m.Lock()
+	defer c.m.Unlock()
+	_, exists := c.pageVisited[targetURL.Host+targetURL.Path]
+	return exists
 }
--- crawler_test.go
+++ crawler_test.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-func TestTaskGetLinksForURL_Success(t *testing.T) {
+func TestCrawler_GetAllLinksFor_Success(t *testing.T) {
 	var linkA *url.URL
 	var linkB *url.URL
 	var linkE *url.URL
@@ -40,43 +40,28 @@ func TestTaskGetLinksForURL_Success(t *testing.T) {
 		assert.NoError(t, err)
 	}))
 
-	requestURL := makeURLFor(t, server.URL)
+	targetURL := makeURLFor(t, server.URL)
 
 	crawler := NewCrawler(http.DefaultClient)
+	linksForTargetURLs, errorForTargetURLs := crawler.GetAllLinksFor(context.Background(), targetURL)
 
-	failedURLs := make(chan *url.URL)
-	linksForURLResults := make(chan *LinksForURLResult)
-
-	go crawler.TaskGetLinksForURL(context.Background(), requestURL, linksForURLResults, failedURLs)
-
-	results := <-linksForURLResults
-
-	close(failedURLs)
-	close(linksForURLResults)
-
-	assert.Contains(t, results.links, linkA)
-	assert.Contains(t, results.links, linkB)
-	assert.Contains(t, results.links, requestURL.ResolveReference(linkE))
+	assert.Nil(t, errorForTargetURLs)
+	assert.Len(t, linksForTargetURLs, 4)
+	assert.Contains(t, linksForTargetURLs[0].links, linkA)
+	assert.Contains(t, linksForTargetURLs[0].links, linkB)
+	assert.Contains(t, linksForTargetURLs[0].links, targetURL.ResolveReference(linkE))
 }
 
-func TestTaskGetLinksForURL_Timeout(t *testing.T) {
+func TestCrawler_GetAllLinksFor_Timeout(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusGatewayTimeout)
 	}))
 
-	requestURL := makeURLFor(t, server.URL)
+	targetURL := makeURLFor(t, server.URL)
 
 	crawler := NewCrawler(&http.Client{Timeout: time.Nanosecond})
+	_, errorForTargetURLs := crawler.GetAllLinksFor(context.Background(), targetURL)
 
-	failedURLs := make(chan *url.URL)
-	linksForURLResults := make(chan *LinksForURLResult)
-
-	go crawler.TaskGetLinksForURL(context.Background(), requestURL, linksForURLResults, failedURLs)
-
-	failedURL := <-failedURLs
-
-	close(failedURLs)
-	close(linksForURLResults)
-
-	assert.Equal(t, requestURL, failedURL)
+	assert.Equal(t, targetURL, errorForTargetURLs[0].targetURL)
+	assert.Error(t, errorForTargetURLs[0].err)
 }
